ec681e90f1a9be4652146bb98dd2be97dc27a96d
Stefano Lazzaroni <stefano.lazzaroni@gmail.com>
2021-02-24 16:56:49 +0100

    Added CI

---
+++ aws-exports.ts
@@ -0,0 +1,9 @@
+const awsmobile = {
+    "aws_project_region": "eu-central-1",
+    "aws_cognito_region": "eu-central-1",
+    "aws_user_pools_id": process.env.AWS_USER_POOLS_ID,
+    "aws_user_pools_web_client_id": process.env.AWS_USER_POOLS_WEB_CLIENT_ID,
+    
+};
+
+export default awsmobile;
