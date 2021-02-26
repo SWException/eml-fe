8788b2f026fc6e911d0fda28a541d43869525ad9
Stefano Lazzaroni <stefano.lazzaroni@gmail.com>
2021-02-26 15:37:58 +0100

    Update CheckoutForm.tsx

--- src/components/CheckoutForm.tsx
+++ src/components/CheckoutForm.tsx
@@ -4,37 +4,35 @@ import {
   useStripe,
   useElements
 } from "@stripe/react-stripe-js";
-
-
 export default function CheckoutForm() {
-
   const [succeeded, setSucceeded] = useState(false);
   const [error, setError] = useState(null);
-  const [processing, setProcessing] = useState(false);
+  const [processing, setProcessing] = useState('');
   const [disabled, setDisabled] = useState(true);
   const [clientSecret, setClientSecret] = useState('');
   const stripe = useStripe();
   const elements = useElements();
-
+  
   useEffect(() => {
     // Create PaymentIntent as soon as the page loads
-    window.fetch("https://5qsqmpfpm8.execute-api.eu-central-1.amazonaws.com/dev/createCharge", {
+    window
+      .fetch("https://5qsqmpfpm8.execute-api.eu-central-1.amazonaws.com/dev/createCharge", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          "Allow-Origin": "*"
+		  'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': true,
         },
-        body: JSON.stringify({items: [{ "id": "demo" }]})
+        body: JSON.stringify({items: [{ id: "xl-tshirt" }]})
       })
       .then(res => {
-        console.log(res);
         return res.json();
       })
       .then(data => {
         setClientSecret(data.clientSecret);
       });
   }, []);
-
+  
   const cardStyle = {
     style: {
       base: {
@@ -52,16 +50,12 @@ export default function CheckoutForm() {
       }
     }
   };
-
   const handleChange = async (event) => {
     // Listen for changes in the CardElement
-
     // and display any errors as the customer types their card details
     setDisabled(event.empty);
     setError(event.error ? event.error.message : "");
   };
-
-
   const handleSubmit = async ev => {
     ev.preventDefault();
     setProcessing(true);
@@ -79,7 +73,6 @@ export default function CheckoutForm() {
       setSucceeded(true);
     }
   };
-
   return (
     <form id="payment-form" onSubmit={handleSubmit}>
       <CardElement id="card-element" options={cardStyle} onChange={handleChange} />
@@ -95,14 +88,14 @@ export default function CheckoutForm() {
           )}
         </span>
       </button>
-      {/* Show any error that happens when processing the payment*/}
+      {/* Show any error that happens when processing the payment */}
       {error && (
         <div className="card-error" role="alert">
           {error}
         </div>
       )}
-      {/* Show a success message upon completion*/}
-      <p>
+      {/* Show a success message upon completion */}
+      <p className={succeeded ? "result-message" : "result-message hidden"}>
         Payment succeeded, see the result in your
         <a
           href={`https://dashboard.stripe.com/test/payments`}
