c3ce668d1b204a2a51b1b8036aca3d5042ba3ad8
Stefano Lazzaroni <stefano.lazzaroni@gmail.com>
2021-03-05 15:33:38 +0100

    Profilo

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -3,6 +3,7 @@ import awsconfig from 'aws-exports';
 import React, { useState } from 'react';
 import { useRouter } from 'next/router';
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import { AddressForm } from 'components/checkout';
 Amplify.configure(awsconfig);
 
 // Salva in automatico i cookie per ricordare se il login è stato fatto
@@ -22,6 +23,17 @@ const Profile: React.FC = ()=>{
 
     const changeNew = (e) =>{
         setNewPass(e.target.value);        
+    }
+
+    const changeEmail = () => {
+        console.log("CHANGING THE MAIL");
+        /*Auth.currentAuthenticatedUser()
+            .then(user => {
+                return Auth.changePassword(user, oldPass, newPass);
+            })
+            .then(data => console.log(data))
+            .catch(err => console.log(err));
+            */
     }
 
     const changePassword = () => {
@@ -48,27 +60,60 @@ const Profile: React.FC = ()=>{
     })
     .catch(err => { setEmail(err); console.log("Errore1: " + err); });
 
-    const onSubmit = () => {
-        router.push('/manageAddresses');
-    }
+
 
     return (
         <div>
-            <button type="button" onClick={()=>onSubmit()}>ADD/EDIT ADDRESSES</button>
-            <Form>
-                <strong>User: {email}</strong>
-                <FormGroup>
-                    <Label for="oldPassword">Old Password</Label>
-                    <Input type="password" onChange={(e)=>{changeOld(e);}} name="password" id="oldPassword" placeholder="Old Password" />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="newPassword">New Password</Label>
-                    <Input type="password" onChange={(e)=>{changeNew(e);}} name="password" id="newPassword" placeholder="New Password" />
-                </FormGroup>
-                <div>
-                    <Button onClick={changePassword}>Change Password</Button>
-                </div>  
-            </Form>
+            <strong>User: {email}</strong>
+            <div>
+                <p>Here you can manage your addresses</p>
+                <p>Add a new one</p>
+                <AddressForm/> 
+                <button type="button">Add</button>
+                <p>Or delete an existing one</p>
+                <select>
+                    <option>Address 1</option>
+                    <option>Address 2</option>
+                    <option>Address 3</option>
+                </select>
+                <button type="button">Delete this address</button>
+            </div>
+            <div>
+                <p>Here you can manage your password</p>
+                <Form>
+                    <FormGroup>
+                        <Label for="oldPassword">Old Password</Label>
+                        <Input type="password" onChange={(e)=>{changeOld(e);}} name="password" id="oldPassword" placeholder="Old Password" />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="newPassword">New Password</Label>
+                        <Input type="password" onChange={(e)=>{changeNew(e);}} name="password" id="newPassword" placeholder="New Password" />
+                    </FormGroup>
+                    <div>
+                        <Button onClick={changePassword}>Change Password</Button>
+                    </div>  
+                </Form>
+            </div>
+            <div>
+                <p>Here you can manage your email</p>
+                <Form>
+                    <FormGroup>
+                        <Label for="newEmail">New Email</Label>
+                        <Input type="email" onChange={(e)=>{changeNew(e);}} name="password" id="newPassword" placeholder="New Email" />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="confirmNewEmail">Confirm New Email</Label>
+                        <Input type="email" onChange={(e)=>{changeNew(e);}} name="password" id="newPassword" placeholder="Confirm New Email" />
+                    </FormGroup>
+                    <div>
+                        <Button onClick={changeEmail}>Change Email</Button>
+                    </div>  
+                </Form>
+            </div>
+            <div>
+                <p>DANGER ZONE!</p>
+                <button type="button">Request account deletion</button>
+            </div>
         </div>
     );
 }
