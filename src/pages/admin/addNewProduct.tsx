c8b298c28304464996a4052f6cbf8eb5ab18d98f
Stefano Lazzaroni <stefano.lazzaroni@gmail.com>
2021-03-05 18:29:12 +0100

    Aggiornata AddNewProduct

---
+++ src/pages/admin/addNewProduct.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+
+const AddNewProduct: React.FC = ({product}) => {
+    let product2 =  {
+        "id": 1,
+        "name": "giochi",
+        "description": "descrizione del prodotto",
+        "photo": "https://www.technogym.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/w/e/wellness_ball_active_sitting_hero_3_3.jpg",
+        "secondaryPhoto": "https://www.technogym.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/w/e/wellness_ball_active_sitting_hero_3_3.jpg",
+        "category": "giochi",
+        "netprice": 5.99,
+        "tax": null
+    };
+
+    return (
+        <>
+            <p>Add new product</p>
+            <form>  
+                <label for="" >Name</label>
+                <input type="text" placeholder="Insert product name"/>
+                <br/>
+                <label for="" >Description</label>
+                <textarea type="text" placeholder="Insert description"/>
+                <br/>
+                <label for="" >Photo</label>
+                <input type="file"/>
+                <br/>
+                <label for="" >Secondary photos (MAX 4)</label>
+                <input type="file"/>
+                <br/>
+                <label for="" >Category</label>
+                <select>
+                    <option>CATEGORY 1</option>
+                    <option>CATEGORY 2</option>
+                    <option>CATEGORY 3</option>
+                    <option>CATEGORY 4</option>
+                    <option>CATEGORY 5</option>
+                    <option>CATEGORY 6</option>
+                </select>
+                <br/>
+                <label for="" >Net price</label>
+                <input type="number" placeholder="Insert product net price"/>
+                <br/>
+                <label for="" >VAT</label>
+                <select>
+                    <option>10%</option>
+                    <option>22%</option>
+                    <option>24%</option>
+                </select>
+                <br/>
+                <label for="" >Visibility</label>
+                <input type="radio" id="visible" name="visibilty" value="visible"/>
+                <label for="male">Visible</label>
+                <input type="radio" id="notVisible" name="visibilty" value="notVisible"/>
+                <label for="female">Not Visible</label>
+                <br/>
+            </form>
+        </>
+    );
+};
+
+export default AddNewProduct;
---
+++ src/pages/admin/productManagement.tsx
@@ -0,0 +1,91 @@
+import React from 'react'
+import { useRouter } from 'next/router';
+
+const ProductManagement: React.FC = ({products}) => {
+    
+    const router = useRouter();
+
+    let products2 = {
+        "products": [
+          {
+            "id": 1,
+            "name": "giochi",
+            "description": "descrizione del prodotto",
+            "photo": "https://www.technogym.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/w/e/wellness_ball_active_sitting_hero_3_3.jpg",
+            "secondaryPhoto": "https://www.technogym.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/w/e/wellness_ball_active_sitting_hero_3_3.jpg",
+            "category": "giochi",
+            "netprice": 5.99,
+            "tax": null
+          },
+          {
+            "id": 2,
+            "name": "cucina"
+          }
+        ]
+      };
+
+    const addNewProduct = () => {
+        router.push('/admin/addNewProduct');
+    }
+
+    return (
+        <>
+            <button type="button" onClick={addNewProduct}>ADD NEW PRODUCT</button>
+            <select>
+                <option>NO CATEGORY FILTER</option>
+                <option>CATEGORY 1</option>
+                <option>CATEGORY 2</option>
+                <option>CATEGORY 3</option>
+                <option>CATEGORY 4</option>
+                <option>CATEGORY 5</option>
+                <option>CATEGORY 6</option>
+            </select>
+            <input type="text" placeholder="Search by name..."/>
+            <button type="button">SEARCH</button>
+            <table>
+                <th>
+                    ID
+                </th>
+                <th>
+                    NAME
+                </th>
+                <th>
+                    NET PRICE
+                </th>
+                <th>
+                    CATEGORY
+                </th>
+                <th>
+                    EDIT
+                </th>
+                <th>
+                    REMOVE
+                </th>
+                {products2.products.map((product)=>(
+                    <tr>
+                        <td>{product.id}</td>
+                        <td>{product.name}</td>
+                        <td>{product.netprice}</td>
+                        <td>{product.category}</td>
+                        <td><button type="button">EDIT</button></td>
+                        <td><button type="button">REMOVE</button></td>
+                    </tr>
+                ))}
+            </table>
+        </>
+    );
+};
+
+export async function getStaticProps() {
+    const res = await fetch('https://virtserver.swaggerhub.com/swexception4/OpenAPI/0.0.1/getProducts');
+
+    const products = await res.json();
+
+    return {
+        props: {
+            products: products,
+        }
+    };    
+}
+
+export default ProductManagement;
