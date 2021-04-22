ecc6cc08b1cdbe9e57564271257c3b0f6321cb07
nikysky <nicole.davanzo97@gmail.com>
2021-04-22 19:34:25 +0200

    fix detail

--- src/pages/detail.tsx
+++ src/pages/detail.tsx
@@ -38,12 +38,6 @@ const Detail: React.FC<Props> = ({product}) => {
     const items = [
         {
         src: 'https://images-na.ssl-images-amazon.com/images/I/61GqztpFduL._AC_UX679_.jpg',
-        },
-        {
-        src: 'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa20%20text%20%7B%20fill%3A%23444%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa20%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23666%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22247.3203125%22%20y%3D%22218.3%22%3ESecond%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E',
-        },
-        {
-        src: 'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa21%20text%20%7B%20fill%3A%23333%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa21%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23555%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22277%22%20y%3D%22218.3%22%3EThird%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E',
         }
     ];
     
@@ -89,7 +83,6 @@ const Detail: React.FC<Props> = ({product}) => {
             height="400" width="400"
         >
             <img src={image} height="400" width="400" alt={'Product Image'} />
-            <CarouselCaption captionText={'Image'} captionHeader={'Image Header'} />
         </CarouselItem>
         );
     });
@@ -126,7 +119,7 @@ const Detail: React.FC<Props> = ({product}) => {
                 <Carousel className={styles.carousel} activeIndex={activeIndex} next={next} previous={previous}>
                             <CarouselIndicators items={items} activeIndex={activeIndex}  color="dark" onClickHandler={goToIndex} />
                             {slides}
-                            <CarouselControl direction="prev" directionText="Previous" onClickHandler={previous} />
+                            <CarouselControl style={{borderColor:"black"}}direction="prev" directionText="Previous" onClickHandler={previous} />
                             <CarouselControl direction="next" directionText="Next" onClickHandler={next} />
                 </Carousel>
                 <div className={styles.productInfo}>
@@ -134,11 +127,11 @@ const Detail: React.FC<Props> = ({product}) => {
                         <div className={styles.productPrice}>Price: € {price}</div>
                         <div className={styles.productDesc}>{description}</div>
                         <div className={styles.productAction}>
-                            <div className={styles.quantity}>
+                            <div>
                                 <button className={styles.plus} onClick={()=>{modifyQuantity('minus')}} type="button" name="button">
                                     -
                                 </button>
-                                <input type="text" name="name" value={_quantity}></input>
+                                <input type="text" name="name" value={_quantity} className={styles.input}></input>
                                 <button className={styles.minus} onClick={()=>{modifyQuantity('plus')}} type="button" name="button">
                                     +
                                 </button>
